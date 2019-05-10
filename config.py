7810fa12c8e749cc3e8aa3b7d5b14282e2702bb7
Myriad-Dreamin <camiyoru@gmail.com>
2019-05-10 14:15:59 +0800

    v0.5.0

---
+++ config.py
@@ -0,0 +1,10 @@
+
+import os.path as parsepath
+
+HOME_PATH = parsepath.dirname(__file__)
+
+HTTP_HEADERS = {'Content-Type': 'application/json'}
+
+INCLUDE_PATH = parsepath.dirname(HOME_PATH) + "/include"
+
+ENC = 'utf-8'
